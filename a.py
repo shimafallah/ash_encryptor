e9d952e71b6655f015c660fac7a3788b4de1cad2
shimaf <fshima02@gmail.com>
2022-02-18 21:14:13 +0330

    add encrypt and decrypt file

--- a.py
+++ a.py
@@ -7,15 +7,14 @@ import hashlib
 import re
 
 
+
 class Encryptor:
-	def __init__(self, key, nonce):
+	def __init__(self, key):
 		self.key = key
-		self.nonce = nonce
 
 	def encrypt(self, message, key):
 		iv = Random.new().read(AES.block_size)
 		cipher = AES.new(key, AES.MODE_GCM, iv)
-		self.nonce = cipher.nonce
 		return iv + cipher.encrypt(message)
 
 	def encrypt_file(self, file_name):
@@ -28,11 +27,11 @@ class Encryptor:
 		with open(f_name + ".ash", 'wb') as fo:
 			fo.write(enc)
 		os.remove(file_name)
-		return f_name
+		
 
 	def decrypt(self, ciphertext, key):
 		iv = ciphertext[:AES.block_size]
-		cipher = AES.new(key, AES.MODE_GCM, iv, nonce=self.nonce)
+		cipher = AES.new(key, AES.MODE_GCM, iv)
 		plaintext = cipher.decrypt(ciphertext[AES.block_size:])
 		return plaintext.rstrip(b"\0")
 
@@ -51,44 +50,54 @@ class Encryptor:
 		with open(regex_m, 'wb') as fo:
 			fo.write(c)
 		os.remove(file_name)
-
-
-parser = argparse.ArgumentParser(description="Encrypt and Decrypt")
-parser.add_argument('func', nargs='?', choices=['e','d'], default='e')
-args, sub_args = parser.parse_known_args()
-
-if args.func == 'e':
-	parser = argparse.ArgumentParser(description='Encrypt')
-	parser.add_argument('-f', nargs=1, help='your file name')
-	parser.add_argument('-p',nargs=1, help='this is your password')
-	args = parser.parse_args(sub_args)
-	for i in args.p:
-		password = i
-	PasswordHash = hashlib.md5(password.encode()).hexdigest()
-	CombinedKey = password + PasswordHash[:32 - len(password)]
-	CombinedKey = CombinedKey.encode()
-	nonce = b'\xfc\xbe\tA\xed\xe4W]\x04O\xc2\xe4\xd3\x83\x9d\x91'
-	enc = Encryptor(CombinedKey, nonce=nonce)
-	if args.f:
-		for i in args.f:
-			x = enc.encrypt_file(i)
-
-elif args.func == 'd':
-	parser = argparse.ArgumentParser(description='Decrypt')
-	parser.add_argument('-f', nargs=1, help='your file name')
-	parser.add_argument('-p',nargs=1, help='this is your password')
-	args = parser.parse_args(sub_args)
-	for i in args.p:
-		password = i
-	PasswordHash = hashlib.md5(password.encode()).hexdigest()
-	CombinedKey = password + PasswordHash[:32 - len(password)]
-	CombinedKey = CombinedKey.encode()
-	nonce = b'\xfc\xbe\tA\xed\xe4W]\x04O\xc2\xe4\xd3\x83\x9d\x91'
-	enc = Encryptor(CombinedKey, nonce)
-	if args.f:
-		for i in args.f:
-			x = enc.decrypt_file(i)
 	
+
+try:
+	parser = argparse.ArgumentParser(description="Encrypt and Decrypt")
+	parser.add_argument('func', nargs='?', choices=['e','d'], default='e')
+	args, sub_args = parser.parse_known_args()
+
+	if args.func == 'e':
+		try:
+			parser = argparse.ArgumentParser(description='Encrypt')
+			parser.add_argument('-f', nargs=1, help='your file name')
+			parser.add_argument('-p',nargs=1, help='this is your password')
+			args = parser.parse_args(sub_args)
+			for i in args.p:
+				password = i
+			PasswordHash = hashlib.md5(password.encode()).hexdigest()
+			CombinedKey = password + PasswordHash[:32 - len(password)]
+			CombinedKey = CombinedKey.encode()
+			enc = Encryptor(CombinedKey)
+			if args.f:
+				for i in args.f:
+					enc.encrypt_file(i)
+		except:
+			print('You must use e -p your_password -f your_name_file')
+
+	elif args.func == 'd':
+		try:
+			parser = argparse.ArgumentParser(description='Decrypt')
+			parser.add_argument('-f', nargs=1, help='your file name')
+			parser.add_argument('-p',nargs=1, help='this is your password')
+			args = parser.parse_args(sub_args)
+			for i in args.p:
+				password = i
+			PasswordHash = hashlib.md5(password.encode()).hexdigest()
+			CombinedKey = password + PasswordHash[:32 - len(password)]
+			CombinedKey = CombinedKey.encode()
+			enc = Encryptor(CombinedKey)
+			if args.f:
+				for i in args.f:
+					enc.decrypt_file(i)
+		except:
+			print('You must use d -p your_password(same the encrypt) -f your_name_file+ash')
+
+
+	else:
+		print('Error')
+except:
+	print('You must follow the instructions')
 
 
 
