cba7ee5dfc1fdadb2555dad2c301c7bb00fe26a7
shimaf <fshima02@gmail.com>
2022-02-18 13:23:38 +0330

    Delete print and rename some variables

--- a.py
+++ a.py
@@ -5,7 +5,6 @@ import os.path
 import argparse
 import hashlib
 import re
-import base64
 
 
 class Encryptor:
@@ -22,15 +21,9 @@ class Encryptor:
 	def encrypt_file(self, file_name):
 		with open(file_name, 'rb') as fo:
 			plaintext = fo.read()
-			# file_byte = f'({file_name})'.encode('utf-16') + plaintext
-		
 		file_encode = f'({file_name})'.encode()
 		file_byte = file_encode + plaintext
-
 		enc = self.encrypt(file_byte, self.key)
-		print('enc', enc[:20])
-
-		# inja bayd ba regex esmo az byte bgirim bedim be ash
 		f_name, f_ext = os.path.splitext(file_name)
 		with open(f_name + ".ash", 'wb') as fo:
 			fo.write(enc)
@@ -40,30 +33,21 @@ class Encryptor:
 	def decrypt(self, ciphertext, key):
 		iv = ciphertext[:AES.block_size]
 		cipher = AES.new(key, AES.MODE_GCM, iv, nonce=self.nonce)
-		# print('cipher', cipher)
 		plaintext = cipher.decrypt(ciphertext[AES.block_size:])
-		# print('plaintext', plaintext)
 		return plaintext.rstrip(b"\0")
 
 	def decrypt_file(self, file_name):
 		with open(file_name, 'rb') as fo:
 			ciphertext = fo.read()
 		dec = self.decrypt(ciphertext, self.key)
-		print(dec[:20])
-		
-		x =str(dec)[2:-1]
-		
+		string_file =str(dec)[2:-1]
 		re_gex = re.compile('(?m)\(([^)]+)\)')
-		regex_match = re_gex.match(x)
+		regex_match = re_gex.match(string_file)
 		regex_m = regex_match[1]
-		dec_file = x.replace(f"({regex_m})", '')
-		print(dec_file[:20])
+		dec_file = string_file.replace(f"({regex_m})", '')
 		dec_file = bytes(dec_file, 'ascii')
-		print(dec_file[:20])
 		c = dec_file.decode('unicode-escape').encode('ISO-8859-1')
-		print(c[:20])
-		
-
+	
 		with open(regex_m, 'wb') as fo:
 			fo.write(c)
 		os.remove(file_name)
